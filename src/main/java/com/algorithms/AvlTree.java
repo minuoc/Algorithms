81c4474b80c937d30448836babe575b089c77c78
clf <clufengc@gmail.com>
2022-01-05 17:37:08 +0800

    一些修改

--- src/main/java/com/algorithms/AvlTree.java
+++ src/main/java/com/algorithms/AvlTree.java
@@ -28,17 +28,48 @@ public class AvlTree<AnyType extends Comparable<? super AnyType>> {
         }else if (compareResult > 0){
             t.right = insert(x, t.right);
         }
-
         return balance(t);
     }
+
+    private int height(AvlNode<AnyType> t) {
+        return t == null ? -1 : t.height;
+    }
+
+    private static final int ALLOWED_IMBALANCE = 1;
+
 
     private AvlNode<AnyType> balance(AvlNode<AnyType> t) {
         if (t == null) {
             return t;
         }
-        if (height(t.left) - height(t.right) )
+        if (height(t.left) - height(t.right) > ALLOWED_IMBALANCE) {
+            if (height(t.left.left) >= height(t.left.right)){
+                t = rotateWithLeftChild(t);
+            }else {
+
+            }
+        }
 
     }
+
+    private AvlNode<AnyType> rotateWithLeftChild(AvlNode<AnyType> k2) {
+        AvlNode<AnyType> k1 = k2.left;
+        k2.left = k1.right;
+        k1.right = k2;
+        k2.height = Math.max(height(k2.left),height(k2.right)) + 1;
+        k1.height = Math.max(height(k1.left),k2.height) + 1;
+        return k1;
+    }
+
+    private AvlNode<AnyType> rotateWithRightChild(AvlNode<AnyType> k1) {
+        AvlNode<AnyType> k2 = k1.right;
+        k1.right = k2.left;
+        k2.left = k1;
+        k1.height = Math.max(height(k1.left),height(k1.right)) + 1;
+        k2.height = Math.max(height(k2.right),k1.height) + 1;
+        return k2;
+    }
+
 
     private static class AvlNode<AnyType> {
 
