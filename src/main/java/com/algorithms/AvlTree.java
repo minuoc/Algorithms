a616e668d56a42c822a1d76a4ae705c4e9877f88
clf <clufengc@gmail.com>
2022-01-17 13:55:49 +0800

    avlTree

--- src/main/java/com/algorithms/AvlTree.java
+++ src/main/java/com/algorithms/AvlTree.java
@@ -1,5 +1,6 @@
 package com.algorithms;
 
+
 public class AvlTree<AnyType extends Comparable<? super AnyType>> {
 
     private AvlNode<AnyType> root;
@@ -8,8 +9,8 @@ public class AvlTree<AnyType extends Comparable<? super AnyType>> {
         root = null;
     }
 
-    public void insert(AnyType x){
-        root = insert(x,root);
+    public void insert(AnyType x) {
+        root = insert(x, root);
     }
 
 
@@ -18,17 +19,110 @@ public class AvlTree<AnyType extends Comparable<? super AnyType>> {
      * @param t 插入的子树的根节点
      * @return 新的子树的根节点
      */
-    private AvlNode<AnyType> insert(AnyType x,AvlNode<AnyType> t) {
+    private AvlNode<AnyType> insert(AnyType x, AvlNode<AnyType> t) {
         if (t == null) {
-            return new AvlNode<>(x,null,null);
+            return new AvlNode<>(x, null, null);
         }
         int compareResult = x.compareTo(t.element);
-        if (compareResult < 0){
+        if (compareResult < 0) {
             t.left = insert(x, t.left);
-        }else if (compareResult > 0){
+        } else if (compareResult > 0) {
             t.right = insert(x, t.right);
         }
         return balance(t);
+    }
+
+
+    public boolean isEmpty(){
+        return root == null;
+    }
+
+    private AnyType findMin() throws Exception {
+        if (isEmpty()){
+            throw new Exception("空");
+        }
+        return findMin(root).element;
+    }
+
+    private AvlNode<AnyType> findMin(AvlNode<AnyType> t) {
+        if (t == null) {
+            return t;
+        }
+        while (t.left != null) {
+            t = t.left;
+        }
+        return t;
+    }
+
+
+
+    private AnyType findMax() throws Exception {
+        if (isEmpty()){
+            throw new Exception("空");
+        }
+        return findMax(root).element;
+    }
+
+    private AvlNode<AnyType> findMax(AvlNode<AnyType> t) {
+        if (t == null) {
+            return t;
+        }
+        while (t.right != null) {
+            t = t.right;
+        }
+        return t;
+    }
+
+    public void remove(AnyType x){
+        root = remove(x,root);
+    }
+
+    private AvlNode<AnyType> remove(AnyType x,AvlNode<AnyType> t){
+        if (t == null) {
+            return t;
+        }
+        int compareResult = x.compareTo(t.element);
+        if (compareResult < 0){
+            t.left = remove(x,t.left);
+        }else if (compareResult > 0){
+            t.right = remove(x,t.right);
+        }else if (t.left != null && t.right != null){
+            //两个children
+            t.element = findMin(t.right).element;
+            t.right = remove(t.element,t.right);
+        }else {
+            t = (t.left != null) ? t.left : t.right;
+        }
+        return balance(t);
+    }
+
+    public boolean contains(AnyType x){
+        return contains(x,root);
+    }
+
+    private boolean contains(AnyType x, AvlNode<AnyType> t) {
+        while (t != null) {
+            int compareResult = x.compareTo(t.element);
+            if (compareResult < 0) {
+                t = t.left;
+            } else if (compareResult > 0) {
+                t = t.right;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void printTree(){
+        printTree(root);
+    }
+    private void printTree(AvlNode<AnyType> t) {
+        if (t != null){
+            printTree(t.left);
+            System.out.println(t.element);
+            printTree(t.right);
+        }
     }
 
     private int height(AvlNode<AnyType> t) {
@@ -39,25 +133,54 @@ public class AvlTree<AnyType extends Comparable<? super AnyType>> {
 
 
     private AvlNode<AnyType> balance(AvlNode<AnyType> t) {
-        if (t == null) {
+        if( t == null ) {
             return t;
         }
-        if (height(t.left) - height(t.right) > ALLOWED_IMBALANCE) {
-            if (height(t.left.left) >= height(t.left.right)){
-                t = rotateWithLeftChild(t);
-            }else {
 
+        if( height( t.left ) - height( t.right ) > ALLOWED_IMBALANCE ) {
+            if( height( t.left.left ) >= height( t.left.right ) ) {
+                t = rotateWithLeftChild( t );
+            } else {
+                t = doubleWithLeftChild( t );
+            }
+        } else if( height( t.right ) - height( t.left ) > ALLOWED_IMBALANCE ) {
+            if( height( t.right.right ) >= height( t.right.left ) ) {
+                t = rotateWithRightChild( t );
+            } else {
+                t = doubleWithRightChild( t );
             }
         }
+        t.height = Math.max( height( t.left ), height( t.right ) ) + 1;
+        return t;
 
     }
+
+    public void checkBalance() {
+        checkBalance(root);
+    }
+
+    private int checkBalance(AvlNode<AnyType> t) {
+        if (t == null) {
+            return -1;
+        }
+        if (t != null) {
+            int hl = checkBalance(t.left);
+            int hr = checkBalance(t.right);
+            if (Math.abs(height(t.left) - height(t.right)) > 1 || height(t.left) != hl || height(t.right) != hr) {
+                System.out.println("OOPS");
+            }
+
+        }
+        return height(t);
+    }
+
 
     private AvlNode<AnyType> rotateWithLeftChild(AvlNode<AnyType> k2) {
         AvlNode<AnyType> k1 = k2.left;
         k2.left = k1.right;
         k1.right = k2;
-        k2.height = Math.max(height(k2.left),height(k2.right)) + 1;
-        k1.height = Math.max(height(k1.left),k2.height) + 1;
+        k2.height = Math.max(height(k2.left), height(k2.right)) + 1;
+        k1.height = Math.max(height(k1.left), k2.height) + 1;
         return k1;
     }
 
@@ -65,9 +188,19 @@ public class AvlTree<AnyType extends Comparable<? super AnyType>> {
         AvlNode<AnyType> k2 = k1.right;
         k1.right = k2.left;
         k2.left = k1;
-        k1.height = Math.max(height(k1.left),height(k1.right)) + 1;
-        k2.height = Math.max(height(k2.right),k1.height) + 1;
+        k1.height = Math.max(height(k1.left), height(k1.right)) + 1;
+        k2.height = Math.max(height(k2.right), k1.height) + 1;
         return k2;
+    }
+
+    private AvlNode<AnyType> doubleWithLeftChild(AvlNode<AnyType> k3) {
+        k3.left = rotateWithRightChild(k3.left);
+        return rotateWithLeftChild(k3);
+    }
+
+    private AvlNode<AnyType> doubleWithRightChild(AvlNode<AnyType> k1) {
+        k1.right = rotateWithLeftChild(k1.right);
+        return rotateWithRightChild(k1);
     }
 
 
@@ -84,10 +217,58 @@ public class AvlTree<AnyType extends Comparable<? super AnyType>> {
         }
 
         public AvlNode(AnyType element, AvlNode<AnyType> lt, AvlNode<AnyType> rt) {
-            element = element;
-            left = lt;
-            right = rt;
-            height = 0;
+            this.element = element;
+            this.left = lt;
+            this.right = rt;
+            this.height = 0;
+        }
+
+    }
+
+
+    public static void main(String[] args) throws Exception {
+        AvlTree<Integer> t = new AvlTree<>();
+        final int SMALL = 40;
+        final int NUMS = 1000000;
+        final int GAP = 37;
+
+        System.out.println( "Checking... (no more output means success)" );
+
+        for( int i = GAP; i != 0; i = ( i + GAP ) % NUMS )
+        {
+            //    System.out.println( "INSERT: " + i );
+            t.insert( i );
+            if( NUMS < SMALL ) {
+                t.checkBalance( );
+            }
+        }
+
+        for( int i = 1; i < NUMS; i+= 2 )
+        {
+            //   System.out.println( "REMOVE: " + i );
+            t.remove( i );
+            if( NUMS < SMALL ) {
+                t.checkBalance( );
+            }
+        }
+        if( NUMS < SMALL ) {
+            t.printTree( );
+        }
+        if( t.findMin( ) != 2 || t.findMax( ) != NUMS - 2 ) {
+            System.out.println( "FindMin or FindMax error!" );
+        }
+
+        for( int i = 2; i < NUMS; i+=2 ) {
+            if( !t.contains( i ) ) {
+                System.out.println( "Find error1!" );
+            }
+        }
+
+        for( int i = 1; i < NUMS; i+=2 )
+        {
+            if( t.contains( i ) ) {
+                System.out.println( "Find error2!" );
+            }
         }
 
     }
--- src/main/java/com/algorithms/BinarySearchTree.java
+++ src/main/java/com/algorithms/BinarySearchTree.java
@@ -178,8 +178,36 @@ public class BinarySearchTree<T extends Comparable<? super T>> {
 
 
     public static void main(String[] args) throws Exception {
+        BinarySearchTree<Integer> t = new BinarySearchTree<>();
+        final int NUMS = 4000;
+        final int GAP = 37;
+        System.out.println("Checking... (no more output means success)");
 
+        for (int i = GAP; i != 0; i = (i + GAP) % NUMS) {
+            t.insert(i);
+        }
 
+        for (int i = 1; i < NUMS; i += 2) {
+            t.remove(i);
+        }
+
+        if (NUMS < 40) {
+            t.printTree();
+        }
+        if (t.findMin() != 2 || t.findMax() != NUMS - 2)
+            System.out.println("FindMin or FindMax error!");
+
+        for (int i = 2; i < NUMS; i += 2) {
+            if (!t.contains(i)) {
+                System.out.println("Find error1!");
+            }
+        }
+
+        for (int i = 1; i < NUMS; i += 2) {
+            if (t.contains(i)) {
+                System.out.println("Find error2!");
+            }
+        }
 
 
     }
